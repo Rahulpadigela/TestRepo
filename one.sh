6082dea9f2331822b1ee71a235aa781441269914
Rahulpadigela <161425621+Rahulpadigela@users.noreply.github.com>
2024-03-19 20:20:07 +0530

    Update one.sh on current manner

--- one.sh
+++ one.sh
@@ -1,1 +1,3 @@
 hello this is my second day to learn the new activities from the git tutorials
+today is the tuesday and it will working very hard to get the result
+so please commit the all changes

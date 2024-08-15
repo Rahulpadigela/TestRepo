f236715f2161aa8a88f8c4880b05c48db1189984
Rahulpadigela <rahul121.prince@gmail.com>
2024-08-15 12:24:45 +0530

    2nd line changes

--- one.sh
+++ one.sh
@@ -5,3 +5,4 @@ hello this is my second day to learn the new activities from the git tutorials
 today is the tuesday and it will working very hard to get the result
 so please commit the all changes
 >>>>>>> 6082dea9f2331822b1ee71a235aa781441269914
+for checking purpose im again adding some ne wlines

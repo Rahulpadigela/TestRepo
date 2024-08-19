108782569ca736acbbfa6a942b6472d404caa567
Rahulpadigela <rahul121.prince@gmail.com>
2024-08-19 16:55:39 +0530

    i written the shell programme

--- one.sh
+++ one.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-hello this is my second day to learn the new activities from the git tutorialstodat is the greatest indepedence day so i would like to learn the new things from the today itself
-=======
-hello this is my second day to learn the new activities from the git tutorials
-today is the tuesday and it will working very hard to get the result
-so please commit the all changes
->>>>>>> 6082dea9f2331822b1ee71a235aa781441269914
-for checking purpose im again adding some ne wlinesplease can you doa help for me
+# im checking this file once agagin to know the status of the github activities
+
+
+echo 'please make sure learn the shell scripting perfectly'
